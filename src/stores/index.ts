70287fd1f14be16bca5db08b0ebad617dd7c6df5
minhck <89894248+minhck@users.noreply.github.com>
2022-11-25 15:40:00 +0700

    Redux toolkit

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,1 +1,14 @@
-export {};
+import { ThunkAction, Action } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { store } from './store';
+
+export type RootDispatch = typeof store.dispatch;
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
+
+export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export { store };
