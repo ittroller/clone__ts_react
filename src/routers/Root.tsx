39294c33e24dfd7c944829ec1e4ba9d4ce6068a7
minhck <89894248+minhck@users.noreply.github.com>
2022-11-28 09:45:38 +0700

    Create router

--- src/routers/Root.tsx
+++ src/routers/Root.tsx
@@ -3,9 +3,10 @@ import { useRoutes } from 'react-router-dom';
 import type { RouteObject } from 'react-router-dom';
 
 import _publicRoutes from './_public';
+import _privateRoutes from './_private';
 
 const RootRouter: React.FC = () => {
-  const rootRouters: RouteObject[] = [..._publicRoutes];
+  const rootRouters: RouteObject[] = [..._publicRoutes, ..._privateRoutes];
 
   return <>{useRoutes(rootRouters)}</>;
 };
--- src/routers/_private.tsx
+++ src/routers/_private.tsx
@@ -1,1 +1,24 @@
-export {};
+import React from 'react';
+import type { RouteObject } from 'react-router-dom';
+
+import { PrivateLayout } from 'src/layouts';
+
+const DashboardScreen = React.lazy(
+  async () => await import('src/screens/privateScreens').then(module => ({ default: module.DashboardScreen })),
+);
+
+const NotFoundScreen = React.lazy(
+  async () => await import('src/screens/NotFound').then(module => ({ default: module.NotFound })),
+);
+
+const _privateRoutes: RouteObject[] = [
+  {
+    element: <PrivateLayout />,
+    children: [
+      { path: '/', element: <DashboardScreen /> },
+      { element: <NotFoundScreen />, path: '*' },
+    ],
+  },
+];
+
+export default _privateRoutes;
