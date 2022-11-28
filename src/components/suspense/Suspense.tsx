7ad45abaecdd4b51690d92e0d2ff861015c70ec7
minhck <89894248+minhck@users.noreply.github.com>
2022-11-28 17:21:07 +0700

    Refactor 2

--- src/components/suspense/Suspense.tsx
+++ src/components/suspense/Suspense.tsx
@@ -1,7 +1,21 @@
+import { Spin } from 'antd';
 import React from 'react';
+import styled from 'styled-components';
 
 const Suspense: React.FC = () => {
-  return <>Loading...</>;
+  return (
+    <SuspenseStyle>
+      <Spin spinning={true} />
+    </SuspenseStyle>
+  );
 };
 
 export default Suspense;
+
+const SuspenseStyle = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
